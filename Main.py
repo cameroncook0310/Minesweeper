46fa8d1bc429272169db6756699dd8c901ab7937
Cameron <cameron.cook0310@gmail.com>
2022-11-08 12:15:24 -0600

    Edited code to reflect changes in Minesweeper.py

---
+++ Main.py
@@ -0,0 +1,29 @@
+import Minesweeper
+
+def main():
+    while True:
+        while True:
+            difficulty = input('What difficulty would you like to play (easy, medium, hard)?: ')
+            if difficulty in('Easy', 'easy', 'Medium', 'medium', 'Hard', 'hard'):
+                game = Minesweeper.Minesweeper(difficulty)
+                game.place_mines()
+                game.generate_tiles()
+                print(game)
+                print('\n')
+                break
+            else:
+                print('Please enter a valid difficulty.')
+            
+        while True:
+            lose = game.select_tile()
+            win = game.win()
+            if win or lose:
+                break
+            else:
+                print(game)
+                print('\n')
+        new_game = input('If you would like to play another game input Y/y, or input any other key to stop playing.')
+        if new_game not in ('Y', 'y'):
+            break
+
+main()
